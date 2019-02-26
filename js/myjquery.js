2ab0d189a517464d08c3122eefdec6ac77c9b1b2
Patricio <concertmate@btinternet.com>
2019-02-26 22:16:33 +0000

    first commit

---
+++ js/myjquery.js
@@ -0,0 +1,157 @@
+
+  $(document).ready(function(){
+     $('#slider ').cycle({ 
+       fx:     'fade', 
+       speed:  'slow',
+       timeoutFn: function (currSlideElement, nextSlideElement, options, forwardFlag) { 
+           return parseInt($(currSlideElement).attr('data-duration'));
+       }   
+     }); 
+  });
+
+
+$(document).ready(function(){
+    $("#descarga").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#descarga").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#min").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#min").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#tep").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#tep").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs1").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs1").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs2").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs2").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs3").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs3").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs4").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs4").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs5").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs5").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs6").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs6").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs7").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs7").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs8").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs8").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs9").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs9").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs10").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs10").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs11").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs11").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+$(document).ready(function(){
+    $("#gigs12").mouseover(function(){
+     $(this).animate({opacity: 0.5}, 400);
+    });
+    $("#gigs12").mouseout(function(){
+         $(this).animate({opacity: 1}, 400);
+    });
+});
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+fjs.parentNode.insertBefore(js,fjs);
+}}(document,"script","twitter-wjs");
+
+
+
+
+
+
+  
